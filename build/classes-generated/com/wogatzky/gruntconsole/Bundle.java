4f23a2b8e9dac744649e38b60ad66930feddf7c5
Oppodelldog <odog@netcologne.de>
2014-12-21 20:36:00 +0100

    basic version

---
+++ build/classes-generated/com/wogatzky/gruntconsole/Bundle.java
@@ -0,0 +1,27 @@
+package com.wogatzky.gruntconsole;
+/** Localizable strings for {@link com.wogatzky.gruntconsole}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Grunt Taskrunner</i>
+     * @see wndTopComponent
+     */
+    static String CTL_wndAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_wndAction");
+    }
+    /**
+     * @return <i>Grunt Taskrunner</i>
+     * @see wndTopComponent
+     */
+    static String CTL_wndTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_wndTopComponent");
+    }
+    /**
+     * @return <i>This is a Grunt Taskrunner window</i>
+     * @see wndTopComponent
+     */
+    static String HINT_wndTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_wndTopComponent");
+    }
+    private void Bundle() {}
+}
